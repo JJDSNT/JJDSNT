d77dcd50ec3d16b6fc4168f20f64a3cf37a4e18a
Jaime Dias <jaimejosediasnt@gmail.com>
2025-06-10 01:05:44 -0300

    Adjust linkedin url translation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { Subject, takeUntil, startWith } from 'rxjs';
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    imports: [TranslocoModule],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.css'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [TranslocoModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-  constructor(private translocoService: TranslocoService) {}
+export class FooterComponent implements OnDestroy {
+  linkedinUrl = 'https://www.linkedin.com/in/jdiasneto/';
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private readonly translocoService: TranslocoService) {
+    this.translocoService.langChanges$
+      .pipe(startWith(this.translocoService.getActiveLang()), takeUntil(this.destroy$))
+      .subscribe(lang => {
+        this.linkedinUrl =
+          lang === 'en'
+            ? 'https://www.linkedin.com/in/jdiasneto/?locale=en_US'
+            : 'https://www.linkedin.com/in/jdiasneto/';
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
